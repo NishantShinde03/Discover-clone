f53652b832931f5b859d03ab29d3d931d19f23bf
Pratik Paulkar <pratik.paulkar@nielseniq.com>
2023-05-08 20:34:56 +0530

    created the data-summary component

---
+++ src/app/data-summary/data-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-summary',
+  templateUrl: './data-summary.component.html',
+  styleUrls: ['./data-summary.component.scss']
+})
+export class DataSummaryComponent {
+
+}
