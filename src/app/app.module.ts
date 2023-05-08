792d814e4aa422a636082a2aabb70696afd3f06e
Shubham Wadhavane <73694931+Shubham770968@users.noreply.github.com>
2023-05-09 01:51:44 +0530

    Design and added functionality for dataset selector (#6)
    Co-authored-by: shubham <19510453.dypit@dypvp.edu.in>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataSummaryComponent } from './data-summary/data-summary.component';
 import { DatasetSelectorComponent } from './dataset-selector/dataset-selector.component';
+import { MatIconModule } from '@angular/material/icon';
 import { BrandbarComponent } from './brandbar/brandbar.component';
 import { ReportPageComponent } from './report-page/report-page.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
