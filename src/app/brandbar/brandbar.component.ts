af625186ad42e873c247af94bed7f169b05184b3
Nishant R Shinde <Nishant.Shinde@nielseniq.com>
2023-05-08 23:49:09 +0530

    brandbar done

--- src/app/brandbar/brandbar.component.ts
+++ src/app/brandbar/brandbar.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-brandbar',
   templateUrl: './brandbar.component.html',
-  styleUrls: ['./brandbar.component.scss']
+  styleUrls: ['./brandbar.component.scss'],
 })
 export class BrandbarComponent {
+  constructor() {
+    console.log(typeof this.user.permissions[0].name);
+  }
 
+  currentlySelected: string = AllPermissions.buildATable;
+
+  //user object
+  user: {
+    uname: string;
+    permissions: Array<{ name: string; route: string }>;
+  } = {
+    uname: 'Nishant.Shinde',
+    permissions: [
+      { name: AllPermissions.monitorMyBusiness, route: '/monitorMyBusiness' },
+      { name: AllPermissions.chooseATemplate, route: 'chooseATemplate' },
+      { name: AllPermissions.buildATable, route: '/buildATable' },
+      { name: AllPermissions.findMyStuff, route: '/findMyStuff' },
+    ],
+  };
+
+  changeCurrentlySelected(permission: string): void {
+    this.currentlySelected = permission;
+  }
+}
+
+enum AllPermissions {
+  monitorMyBusiness = 'Monitor my business',
+  chooseATemplate = 'Choose a template',
+  buildATable = 'Build a table',
+  findMyStuff = 'Find my stuff',
 }
