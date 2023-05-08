b5e984f4db1bc5fd21890fdcfabb8a1e2dc41d2c
shubham <19510453.dypit@dypvp.edu.in>
2023-05-08 21:02:38 +0530

    generated dataset-selector component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataSummaryComponent } from './data-summary/data-summary.component';
+import { DatasetSelectorComponent } from './dataset-selector/dataset-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataSummaryComponent
+    DataSummaryComponent,
+    DatasetSelectorComponent
   ],
   imports: [
     BrowserModule,
