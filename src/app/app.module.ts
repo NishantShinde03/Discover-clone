ed7d58487d9cfc43b7422f1c3f7c40a0b946e256
Pratik Paulkar <pratik.paulkar@nielseniq.com>
2023-05-09 00:15:07 +0530

    data-summary component ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataSummaryComponent } from './data-summary/data-summary.component';
@@ -12,7 +12,8 @@ import { DataSummaryComponent } from './data-summary/data-summary.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
